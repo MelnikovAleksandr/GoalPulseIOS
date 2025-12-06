2d3aa721214a0763afc597a16a8516ee19281966
MelnikovAleksandr <melnikovjavadev@gmail.com>
2025-12-06 17:02:12 +0500

    Initial Commit

---
+++ GoalPulse/GoalPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalPulseApp.swift
+//  GoalPulse
+//
+//  Created by Александр Мельников on 06.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct GoalPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
